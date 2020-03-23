dbe4e6a3321d7d45d49f173d7baadf6546e0fcc6
Rafael Dalprá <thedalpra@gmail.com>
2020-03-23 11:13:08 +0000

    Added a few improvements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Fetch } from "react-request";
 import { Spinner } from "react-bootstrap";
-import { Row, Col, Card, Table, Dropdown } from "react-bootstrap";
+import { Row, Col, Card, Table } from "react-bootstrap";
 import NumberFormat from "react-number-format";
 
 const selectStyles = {
@@ -228,15 +228,43 @@ const Today = () => {
                               <td>
                                 <Link to={{ pathname: `/${item.country}` }}>
                                   {item.country}
-                                </Link>
+                                </Link>{" "}
+                                <small>
+                                  (
+                                  <small style={{ color: "#4271b3" }}>
+                                    +
+                                    {Number(
+                                      (Number(item.todayCases) /
+                                        Number(item.cases)) *
+                                        100
+                                    ).toFixed(1)}
+                                    %
+                                  </small>{" "}
+                                  <small style={{ color: "#ff3030" }}>
+                                    +
+                                    {Number(
+                                      (Number(item.todayDeaths) /
+                                        Number(item.deaths)) *
+                                        100
+                                    ).toFixed(1)}
+                                    %
+                                  </small>
+                                  )
+                                </small>
                               </td>
                               <td
-                                style={{ textAlign: "right", color: "#4271b3" }}
+                                style={{
+                                  textAlign: "center",
+                                  color: "#4271b3"
+                                }}
                               >
                                 {item.todayCases}
                               </td>
                               <td
-                                style={{ textAlign: "right", color: "#ff3030" }}
+                                style={{
+                                  textAlign: "center",
+                                  color: "#ff3030"
+                                }}
                               >
                                 <strong>{item.todayDeaths}</strong>
                               </td>
